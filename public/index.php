1694ffe88089504f26f940853aa31953d1261375
Naman <namankumar80510@gmail.com>
2025-06-06 16:01:12 +0530

    vanilla router

--- public/index.php
+++ public/index.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
-echo "This tuts rocks!";
+$uri = $_SERVER['REQUEST_URI'];
+
+echo match ($uri) {
+    '/' => 'This tuts rocks!!',
+    '/about' => 'This is the about page!',
+    '/contact' => 'This is the contact page!',
+    default => '404',
+};
